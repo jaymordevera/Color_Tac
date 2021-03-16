94e724546b631d27afbfc60b2b1e67876ab7ac25
Jaymor De Vera <60604350+jaymordevera@users.noreply.github.com>
2021-03-16 12:08:19 +0800

    Upload main file

---
+++ main.py
@@ -0,0 +1,159 @@
+import pygame
+import os
+import random
+
+pygame.init()
+pygame.font.init()
+
+# Constant Variables
+DIMENSIONS = (380,410)
+SCREEN = pygame.display.set_mode(DIMENSIONS)
+TITLE = pygame.display.set_caption("Tic Tac Toe")
+FPS = 60
+WHITE = (255, 255, 255)
+BLACK = (30, 30, 30)
+GREEN = (106,177,135)
+BLUE = (0,145,213)
+GREY = (100, 100, 100)
+WINNER_FONT = pygame.font.SysFont('comicsans', 60)
+
+# Players
+player = [GREEN, BLUE]
+player_win = ['GREEN WON!', 'BLUE WON!']
+n = random.randint(0, 1)
+turn = player[n]
+is_winner = False
+
+# Squares
+SQUARE_WIDTH, SQUARE_HEIGHT = 100, 100
+
+lst_squares = [pygame.Rect(20,50, SQUARE_WIDTH, SQUARE_HEIGHT),
+               pygame.Rect(140,50, SQUARE_WIDTH, SQUARE_HEIGHT),
+               pygame.Rect(260,50, SQUARE_WIDTH, SQUARE_HEIGHT),
+               
+               pygame.Rect(20,170, SQUARE_WIDTH, SQUARE_HEIGHT),
+               pygame.Rect(140,170, SQUARE_WIDTH, SQUARE_HEIGHT),
+               pygame.Rect(260,170, SQUARE_WIDTH, SQUARE_HEIGHT),
+               
+               pygame.Rect(20,290, SQUARE_WIDTH, SQUARE_HEIGHT),
+               pygame.Rect(140,290, SQUARE_WIDTH, SQUARE_HEIGHT),
+               pygame.Rect(260,290, SQUARE_WIDTH, SQUARE_HEIGHT),
+               ]
+
+square_color = []
+
+# Draw a board in the screen
+def draw_board():
+    SCREEN.fill(WHITE)
+    
+    for square in range(9):
+        pygame.draw.rect(SCREEN, BLACK, lst_squares[square])
+        square_color.append(BLACK)
+    
+    pygame.display.update()
+
+
+# Check the board if there's a winner
+def check_board():
+    global is_winner
+    
+    def check_horizontally(i):
+        global is_winner
+        try:
+            if square_color[i] == square_color[i+1] and square_color[i] == square_color[i+2] and square_color[i] != BLACK:
+                is_winner = True
+        except:
+            pass
+        
+    def check_vertically(i):
+        global is_winner
+        try:
+            if square_color[i] == square_color[i+3] and square_color[i] == square_color[i+6] and square_color[i] != BLACK:
+                is_winner = True
+        except:
+            pass
+    
+    def check_diagonally(i):   
+        global is_winner
+        
+        try:
+            if square_color[i] == square_color[i+4] and square_color[i] == square_color[i+8] and square_color[i] != BLACK:
+                is_winner = True
+            
+            if square_color[i] == square_color[i+2] and square_color[i] == square_color[i+4] and square_color[i] != BLACK:
+                is_winner = True
+                
+        except:
+            pass
+    
+    for index in range(9):
+        check_vertically(index)
+        check_horizontally(index)
+        check_diagonally(index)
+        
+        
+# When clicked, the color changes
+def change_color(index):
+    global turn
+        
+    if square_color[index] == BLACK:
+        pygame.draw.rect(SCREEN, turn, lst_squares[index])
+        
+        if turn == BLUE:
+            turn = GREEN
+        else:
+            turn = BLUE
+            
+        square_color.pop(index)
+        square_color.insert(index, turn)
+    
+    pygame.display.update()
+    
+# Draw the winner
+def draw_winner(winner):
+    draw_winner = WINNER_FONT.render(winner, 1, GREY)
+    SCREEN.blit(draw_winner, (DIMENSIONS[0]/2 - draw_winner.get_width()/2, 5))
+
+def draw_turn(turn):
+    draw_turn = WINNER_FONT.render(turn, 1, GREY)
+    SCREEN.blit(draw_turn, (DIMENSIONS[0]/2 - draw_turn.get_width()/2, 5))
+    pygame.display.update()
+
+# Main Function
+def main():
+    """Main function of the game"""
+    global square_color, is_winner
+    
+    draw_board()
+    
+    run_status = True
+    while run_status:
+        pygame.time.Clock().tick(FPS)
+            
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run_status = False
+                pygame.quit()
+        
+            if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                pos_mouse = pygame.mouse.get_pos()
+                for index, square in enumerate(lst_squares):
+                    if square.collidepoint(pos_mouse):
+                        change_color(index)
+                        check_board()
+                        
+                        
+                        if is_winner == True:
+                            winner = player.index(turn) - 1
+                            pygame.time.delay(1000)
+                            draw_winner(player_win[winner])
+                            pygame.display.update()
+                            pygame.time.delay(2000)
+                            square_color = []
+                            is_winner = False
+                            main()
+        
+        
+if __name__ == '__main__':
+    main()
+    
